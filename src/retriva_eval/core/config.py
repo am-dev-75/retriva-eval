c06b839b12d7335bc29b433420622d7d51572bbd
Andrea Marson <am.dev.75@gmail.com>
2026-06-02 09:57:05 +0200

    Version 1.0.0-rc0.

--- src/retriva_eval/core/config.py
+++ src/retriva_eval/core/config.py
@@ -16,7 +16,7 @@ from typing import Optional, Dict
 from pydantic_settings import BaseSettings, SettingsConfigDict
 import json
 
-VERSION = "0.1.0"
+VERSION = "1.0.0-rc0"
 
 class Settings(BaseSettings):
     # Retriva Client
